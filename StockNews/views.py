689ed0579f8b93f307787d429b54b5a0a85f5ea9
ajmal-kh <khajmal68@gmail.com>
2021-10-03 02:21:08 +0530

    Integrated with UI(Not accepting input from the user) written python script to calculate and visualize the sentiment score. Score is being displayed as an image on the webpage.

---
+++ StockNews/views.py
@@ -0,0 +1,70 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+#import nltk then in console type nltk.download() and download vader_lexicon from the gui
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#return home page
+def greetings(request):
+    res = render(request,'StockNews/home.html')
+    return res
+
+# return home page with visualization
+def search(request):
+    
+
+    finviz_url = 'https://finviz.com/quote.ashx?t='
+    tickers = ['AMZN']
+
+    news_tables = {}
+    for ticker in tickers:
+        url = finviz_url + ticker
+
+        req = Request(url=url, headers={'user-agent': 'my-app'})
+        response = urlopen(req)
+
+        html = BeautifulSoup(response, features='html.parser')
+        news_table = html.find(id='news-table')
+        news_tables[ticker] = news_table
+
+    parsed_data = []
+
+    for ticker, news_table in news_tables.items():
+
+        for row in news_table.findAll('tr'):
+
+            title = row.a.text
+            date_data = row.td.text.split(' ')
+
+            if len(date_data) == 1:
+                time = date_data[0]
+            else:
+                date = date_data[0]
+                time = date_data[1]
+
+            parsed_data.append([ticker, date, time, title])
+    print(parsed_data);
+    df = pd.DataFrame(parsed_data, columns=['ticker', 'date', 'time', 'title'])
+
+    vader = SentimentIntensityAnalyzer()
+
+    f = lambda title: vader.polarity_scores(title)['compound']
+    df['compound'] = df['title'].apply(f)
+    df['date'] = pd.to_datetime(df.date).dt.date
+
+    # plt.figure(figsize=(10,8))
+    mean_df = df.groupby(['ticker', 'date']).mean().unstack()
+    mean_df = mean_df.xs('compound', axis="columns")
+    plot = mean_df.plot(figsize=(10,8),kind='bar')
+    # plt.show()
+    fig = plot.get_figure()
+    fig.savefig("static/images/output.png")
+
+    # res = render(request,'StockNews/home.html')
+    res = render(request,'StockNews/home.html',{"img_name":"output.png"})
+    return res
